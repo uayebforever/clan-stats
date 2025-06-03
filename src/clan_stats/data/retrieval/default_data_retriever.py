e2b0e66d3217c044315e27f650561ffbdd0a5830
uayeb <uayebforever@gmail.com>
2025-06-03 17:31:47 -0600

    Change to Bungio default data retriever.

--- src/clan_stats/data/retrieval/default_data_retriever.py
+++ src/clan_stats/data/retrieval/default_data_retriever.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 from clan_stats.config import ClanStatsConfig
 from .aiobungie_rest_data_retriever import AioBungieRestDataRetriever
+from .bungio_data_retriever import BungioDataRetriever
 from .cached_data_retriever import CachedDataRetriever
 from .data_retriever import DataRetriever
 
 
 def get_default_data_retriever(config: ClanStatsConfig) -> DataRetriever:
     # return AioBungieRestDataRetriever(config.bungie_api_key)
-    return CachedDataRetriever(
-        delegate=AioBungieRestDataRetriever(config.bungie_api_key),
-        database_directory=Path(".").joinpath("cache"))
+    # return CachedDataRetriever(
+    #     delegate=AioBungieRestDataRetriever(config.bungie_api_key),
+    #     database_directory=Path(".").joinpath("cache"))
+    return BungioDataRetriever(config.bungie_api_key)
