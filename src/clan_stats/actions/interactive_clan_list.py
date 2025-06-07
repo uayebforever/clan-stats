bb313366b0a1a6b888ed6dbb1041429ef1cac783
uayeb <uayebforever@gmail.com>
2025-06-07 08:00:34 -0600

    Most of the edit functionality in place.

--- src/clan_stats/actions/interactive_clan_list.py
+++ src/clan_stats/actions/interactive_clan_list.py
@@ -1,15 +1,17 @@
 import asyncio
 import logging
 from datetime import date
-from typing import Tuple, Callable, Optional, Dict, NamedTuple
+from typing import Tuple, Callable, Optional, Dict, NamedTuple, Sequence, ClassVar, TypeVar, Generic, ParamSpec
 
 from textual import events
 from textual.app import App, ComposeResult
 from textual.containers import Vertical, Horizontal, Grid
+from textual.coordinate import Coordinate
 from textual.reactive import reactive
+from textual.screen import Screen, ModalScreen
 from textual.widgets import DataTable, Header, Footer, Label, Rule, Input, TextArea
 
-from clan_stats.clan_manager import ClanMembershipDatabase, AccountType, Member, Account
+from clan_stats.clan_manager import ClanMembershipDatabase, AccountType, Member, Account, MembershipStatus
 from clan_stats.clan_manager.clan_membership_database import find_unknown_players
 from clan_stats.clan_manager.membership_database import MembershipDatabase
 from clan_stats.data._bungie_api.bungie_enums import GameMode
@@ -22,6 +24,8 @@ from clan_stats.util.optional import require_else
 EDITING = "editing"
 
 log = logging.getLogger(__name__)
+
+P = ParamSpec("P")
 
 
 def interactive_clan_list(clan_id: int, data_retriever: DataRetriever):
@@ -37,13 +41,236 @@ class UnknownPlayersTable(DataTable, can_focus=True):
     ]
 
 
-class EscInput(Input):
+class MembersTable(DataTable, can_focus=True):
+    BINDINGS = [
+        ("e", 'app.edit', "Edit member")
+    ]
+
+
+class EscapableInput(Input):
+    """Input that bubbles the escape key up rather than eating it."""
 
     def on_key(self, event: events.Key):
         if event.key != "escape":
             super()._on_key(event)
 
-class MemberEditor(Vertical):
+
+_R = TypeVar('_R')
+
+
+class UpdatingTable(DataTable, Generic[_R]):
+    COLUMNS: ClassVar[Sequence[Tuple[str, Callable[[_R], str]]]] = []
+
+    def on_mount(self):
+        for column_head, _ in self.COLUMNS:
+            self.add_column(column_head)
+        self.cursor_type = "row"
+
+    def update(self, row_list: Sequence[_R]) -> None:
+        self.clear()
+        self._row_sources = {}
+        for i, row in enumerate(row_list):
+            row_key = self.add_row(*(lam(row) for _, lam in self.COLUMNS),
+                                   key=str(i))
+            self._row_sources[row_key] = row
+
+    def selected_object(self) -> _R:
+        return self._row_sources[self.coordinate_to_cell_key(self.cursor_coordinate).row_key]
+
+
+class SavableInput(EscapableInput):
+
+    def __init__(self, label: str, save_hook: Callable[[str], None], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.save_hook = save_hook
+        self.label = label
+
+    def on_mount(self):
+        self.border_title = self.label
+
+    def save(self):
+        self.save_hook(self.value)
+
+
+class EditMemberHistoryScreen(ModalScreen):
+    BINDINGS = [
+        ('ctrl+s', 'save', "Save changes"),
+        ('escape', 'cancel', "Cancel without saving changes")
+    ]
+
+    def __init__(self, status: MembershipStatus, **kwargs):
+        super().__init__(**kwargs)
+        self.status = status
+
+    def on_mount(self):
+        self.add_class("modal")
+
+    def compose(self) -> ComposeResult:
+        def save_status(value: str):
+            self.status.status = value
+
+        def save_date(value: str):
+            self.status.date_conferred = date.fromisoformat(value)
+
+        def save_notes(value: str):
+            self.status.notes = value
+
+        yield Header()
+        with Grid(classes="modal") as g:
+            yield Label(f"Editing Membership status")
+            yield SavableInput(
+                value=self.status.status,
+                label="Status",
+                save_hook=save_status)
+            yield SavableInput(
+                value=self.status.date_conferred.isoformat(),
+                label="Date Conferred",
+                save_hook=save_date
+            )
+
+            yield SavableInput(
+                value=self.status.notes,
+                label="Notes",
+                save_hook=save_notes
+            )
+        yield Footer()
+
+    def action_save(self):
+        inputs = self.query(SavableInput)
+        for input in inputs:
+            input.save()
+        self.dismiss("save")
+
+    def action_cancel(self):
+        self.dismiss("cancel")
+
+
+class EditMemberAccountsScreen(ModalScreen):
+    BINDINGS = [
+        ('ctrl+s', 'save', "Save changes"),
+        ('escape', 'cancel', "Cancel without saving changes")
+    ]
+
+    def __init__(self, account: Account, **kwargs):
+        super().__init__(**kwargs)
+        self._account = account
+
+    def compose(self) -> ComposeResult:
+        def save_type(value: str):
+            self._account.account_type = value
+        def save_name(value: str):
+            self._account.name = value
+        def save_id(value: str):
+            self._account.account_identifier = value
+        def save_note(value: str):
+            self._account.note = value
+
+        yield Header()
+        with Grid(classes="modal") as g:
+            yield Label(f"Editing Member Account")
+            yield SavableInput(
+                value=self._account.account_type,
+                label="Account Type",
+                save_hook=save_type)
+            yield SavableInput(
+                value=self._account.name,
+                label="Display Name",
+                save_hook=save_name
+            )
+            yield SavableInput(
+                value=self._account.account_identifier,
+                label="Account Unique Identifier",
+                save_hook=save_id
+            )
+            yield SavableInput(
+                value=self._account.note,
+                label="Notes",
+                save_hook=save_note
+            )
+        yield Footer()
+
+    def action_save(self):
+        inputs = self.query(SavableInput)
+        for input in inputs:
+            input.save()
+        self.dismiss("save")
+
+    def action_cancel(self):
+        self.dismiss("cancel")
+
+
+class MemberHistoryTable(UpdatingTable):
+    COLUMNS: ClassVar[Sequence[Tuple[str, Callable[[MembershipStatus], str]]]] = (
+        ("Status", lambda m: m.status),
+        ("Date Conferred", lambda m: m.date_conferred),
+        ("Notes", lambda m: truncate_str(require_else(m.notes, ""), 20)),
+    )
+
+    BINDINGS = [
+        ('e', 'edit', "Edit"),
+        ('a', 'add', "Add new status"),
+    ]
+
+    def __init__(self, member: Member, **kwargs):
+        super().__init__(**kwargs)
+        self.member = member
+
+    def on_mount(self):
+        self.border_title = "Member History"
+
+    def action_edit(self):
+        self.app.push_screen(EditMemberHistoryScreen(self.selected_object()),
+                             callback=lambda result: self.update(self.member.membership_history))
+
+    def action_add(self):
+        new_status = MembershipStatus(
+            member=self.member,
+            status="",
+            date_conferred=date.today(),
+        )
+        self.add_row(new_status)
+        self.app.push_screen(EditMemberHistoryScreen(new_status),
+                             callback=lambda result: self.update(self.member.membership_history))
+
+
+class MemberAccountsTable(UpdatingTable):
+    COLUMNS: ClassVar[Sequence[Tuple[str, Callable[[Account], str]]]] = [
+        ("Type", lambda a: a.account_type),
+        ("Name", lambda a: a.name),
+        ("Act", lambda a: "A" if a.is_active else ""),
+        ("Notes", lambda a: a.note)
+    ]
+
+    BINDINGS = [
+        ('e', 'edit', "Edit"),
+        ('a', 'add', "Add new status"),
+    ]
+
+    def __init__(self, member: Member, **kwargs):
+        super().__init__(**kwargs)
+        self.member = member
+
+    def on_mount(self):
+        self.border_title = "Accounts"
+
+    def action_edit(self):
+        self.app.push_screen(EditMemberAccountsScreen(self.selected_object()),
+                             callback=lambda result: self.update(self.member.accounts))
+
+    def action_add(self):
+        new_status = Account(
+            member=self.member,
+            account_type="",
+            name="",
+            account_identifier="",
+            is_active=False
+        )
+        self.add_row(new_status)
+        self.app.push_screen(EditMemberAccountsScreen(new_status),
+                             callback=lambda result: self.update(self.member.accounts))
+
+
+class MemberEditor(Screen):
     BINDINGS = [
         ("ctrl+s", 'save', "Save member"),
         ("escape", 'escape', "Cancel and return to list"),
@@ -53,15 +280,18 @@ class MemberEditor(Vertical):
     bungie_id = reactive("")
     discord_name = reactive("")
 
-    def __init__(self, clan_database: ClanMembershipDatabase, *args, **kwargs):
+    def __init__(self, clan_database: ClanMembershipDatabase, member: Member, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._clan_database = clan_database
+        self._member: Member = member
+
         self._grid: Optional[Grid] = None
         self._fields: Dict[str, Input] = {}
-        self._member: Optional[Member] = None
+        self.history_table: Optional[MemberHistoryTable] = None
+        self.accounts_table: Optional[MemberAccountsTable] = None
 
     def add_field(self, title: str, /, id: str, disabled: bool = False):
-        field = EscInput(id=id, disabled=disabled)
+        field = EscapableInput(id=id, disabled=disabled)
         field.border_title = title
         if title in self._fields:
             raise ValueError(f"Field {title} already exists!")
@@ -69,14 +299,32 @@ class MemberEditor(Vertical):
         return field
 
     def compose(self) -> ComposeResult:
-        with Grid(id="basics") as grid:
-            grid.styles.grid_size_columns = 2  # Two columns
-            grid.styles.grid_rows = (3,)  # three lines per row
-            yield self.add_field("Bungie Name", id="bungie_name", disabled=True)
-            yield self.add_field("Discord Username", id="discord_name")
-            yield self.add_field("Bungie Membership Number", id="bungie_id", disabled=True)
-            yield self.add_field("Date joined", id="joined")
-        yield TextArea(id="member_notes")
+        yield Header()
+        with Vertical() as base_vert:
+            with Horizontal() as grid:
+                with Vertical():
+                    yield self.add_field("Bungie Name", id="bungie_name", disabled=True)
+                    yield self.add_field("Discord Username", id="discord_name")
+                    yield self.add_field("Bungie Membership Number", id="bungie_id", disabled=True)
+                    yield self.add_field("Date joined", id="joined")
+                yield TextArea(id="member_notes")
+            with Horizontal() as horiz:
+                self.history_table = MemberHistoryTable(self._member)
+                yield self.history_table
+                self.accounts_table = MemberAccountsTable()
+                yield self.accounts_table
+        yield Footer()
+
+    def on_mount(self):
+        bungie = only(self._member.active_accounts(AccountType.BUNGIE))
+        discord = only(self._member.active_accounts(AccountType.DISCORD))
+        self.bungie_name = bungie.name
+        self.discord_name = discord.account_identifier
+        self.bungie_id = bungie.account_identifier
+        self._fields["joined"].value = self._member.first_join.isoformat()
+
+        self.history_table.update(self._member.membership_history)
+        self.accounts_table.update(self._member.accounts)
 
     def watch_bungie_name(self, bungie_name: str):
         self._fields["bungie_name"].value = bungie_name
@@ -88,32 +336,23 @@ class MemberEditor(Vertical):
         self._fields["bungie_id"].value = bungie_id
 
     def action_save(self):
-        if self._member is None:
-            raise RuntimeError
         discord_account = only(self._member.active_accounts(AccountType.DISCORD))
         discord_account.name = self._fields["discord_name"].value
         discord_account.account_identifier = self._fields["discord_name"].value
         self._clan_database.save_changes()
-        self.run_action('app.end_edit')
-        self._member = None
+        self.dismiss()
 
     def action_escape(self):
         self._clan_database.cancel_chages()
-        self._member = None
-        self.run_action('app.end_edit')
-
-    def action_update(self, member: Member):
-        bungie = only(member.active_accounts(AccountType.BUNGIE))
-        discord = only(member.active_accounts(AccountType.DISCORD))
-        self._member = member
-        self.bungie_name = bungie.name
-        self.discord_name = discord.account_identifier
-        self.bungie_id = bungie.account_identifier
-        self._fields["joined"].value = member.first_join.isoformat()
+        self.dismiss()
 
 
 class InteractiveClanList(App):
     CSS_PATH = "interactive_clan_list.tcss"
+
+    BINDINGS = [
+        ('q', 'quit', "Exit"),
+    ]
 
     _active_columns: Tuple[str, Callable[[Member], str]] = (
         ("Bungie Name", lambda m: only(m.active_accounts(AccountType.BUNGIE)).name),
@@ -142,34 +381,40 @@ class InteractiveClanList(App):
                 yield UnknownPlayersTable(id="unknown")
             with Vertical(classes="area"):
                 yield Label("Current Members")
-                yield DataTable(id="active")
+                yield MembersTable(id="active")
             with Vertical(classes="area"):
                 yield Label("Past Members")
-                yield DataTable(id="past")
-        yield MemberEditor(self._clan_database, id="edit")
+                yield MembersTable(id="past")
         yield Footer()
 
     def on_mount(self) -> None:
         self._populate_unknown()
         self._populate_active()
 
-    def action_add(self):
-        table = self.query_one("#unknown", DataTable)
+    async def action_add(self):
+        table = self.query_one("#unknown", UnknownPlayersTable)
         key = table.coordinate_to_cell_key(table.cursor_coordinate).row_key
         player_to_add = only(p for p in self._clan.players if str(p.primary_membership.membership_id) == key)
         log.info("Adding player % to clan", player_to_add)
         member = self._clan_database.new_member(
             bungie_primary_membership_id=player_to_add.primary_membership.membership_id,
             bungie_display_name=player_to_add.name,
-            discord_username=f"discord_{player_to_add.name}",
+            discord_username="",
             join_date=player_to_add.group_join_date.date()
         )
 
-        self.edit_member(member)
-        # Edit member will call updates if the edit is saved.
+        await self.edit_member(member)
+
+    async def action_edit(self):
+        table = self.query_one("#active", MembersTable)
+        key = table.coordinate_to_cell_key(table.cursor_coordinate).row_key
+        member = only(m for m in self._clan_database.all_members() if str(m.id) == key)
+
+        await self.edit_member(member)
 
     def _populate_unknown(self):
         table = self.query_one("#unknown", DataTable)
+        table.cursor_type = 'row'
         for column_name, _ in self._unknown_columns:
             table.add_column(column_name)
 
@@ -178,13 +423,20 @@ class InteractiveClanList(App):
     def _update_unknown(self):
         unknown_players = find_unknown_players(self._clan_database, self._clan)
         table = self.query_one("#unknown", DataTable)
+        prev_coord = table.cursor_coordinate
+        table.clear()
 
         for player in unknown_players:
             table.add_row(*(lam(player) for _, lam in self._unknown_columns),
                           key=str(player.primary_membership.membership_id))
+        if prev_coord.row > table.row_count - 1:
+            table.cursor_coordinate = Coordinate(table.row_count - 1, 0)
+        else:
+            table.cursor_coordinate = prev_coord
 
     def _populate_active(self):
         table = self.query_one("#active", DataTable)
+        table.cursor_type = 'row'
 
         for column_name, _ in self._active_columns:
             table.add_column(column_name)
@@ -193,12 +445,19 @@ class InteractiveClanList(App):
 
     def _update_active(self):
         table = self.query_one("#active", DataTable)
+        prev_coord = table.cursor_coordinate
         table.clear()
+
         for member in self._clan_database.current_members():
-            table.add_row(*(lam(member) for _, lam in self._active_columns))
+            table.add_row(*(lam(member) for _, lam in self._active_columns), key=str(member.id))
+        if prev_coord.row > table.row_count - 1:
+            table.cursor_coordinate = Coordinate(table.row_count - 1, 0)
+        else:
+            table.cursor_coordinate = prev_coord
 
     def _populate_past(self):
         table = self.query_one("#past", DataTable)
+        table.cursor_type = 'row'
 
         columns = (
             ("Bungie Name", lambda m: only(m.active_accounts(AccountType.BUNGIE)).name),
@@ -213,18 +472,14 @@ class InteractiveClanList(App):
         for member in self._clan_database.past_members():
             table.add_row(*(lam(member) for _, lam in columns))
 
-    def edit_member(self, member: Member):
-        # self.query_one("#list", Vertical).add_class("editing")
-        editor = self.query_one("#edit", MemberEditor)
-        # editor.add_class("editing")
-        self.add_class(EDITING)
-        editor.action_update(member)
+    def _update_all(self):
+        self._update_unknown()
+        self._update_active()
 
-    def action_end_edit(self):
-        # self.query_one("#list", Vertical).remove_class(EDITING)
-        editor = self.query_one("#edit", MemberEditor)
-        # editor.remove_class("editing")
-        self.remove_class(EDITING)
+    async def edit_member(self, member: Member):
+        await self.push_screen(
+            MemberEditor(self._clan_database, member=member),
+            callback=lambda resut: self._update_all())
 
 
 def truncate_str(text: str, length: int) -> str:
