26ac3f1054bcab2525c022f630d8f4b99fe02d73
Andy Green <uayebforever@gmail.com>
2025-08-29 22:49:54 -0600

    Add Desert Perpetual to Raid Report.

--- src/clan_stats/actions/raid_report.py
+++ src/clan_stats/actions/raid_report.py
@@ -62,11 +62,13 @@ class Raid(StrEnum):
     PAN = "Pantheon"
     COS = "Crown of Sorrow"
     SOTP = "Scourge of the Past"
+    DPS = "The Desert Perpetual: Standard"
+    DPC = "The Desert Perpetual: Contest"
     UNKNOWN = "Unknown?"
 
     @classmethod
     def current_raids(cls) -> Sequence['Raid']:
-        return [cls.SE, cls.CROTA, cls.ROOT, cls.KF, cls.VOW, cls.VOG, cls.DSC, cls.GOS, cls.LW]
+        return [cls.DPS, cls.SE, cls.CROTA, cls.ROOT, cls.KF, cls.VOW, cls.VOG, cls.DSC, cls.GOS, cls.LW]
 
     @classmethod
     def from_director_activity_hash(cls, dah: int, manifest: Manifest) -> 'Raid':
