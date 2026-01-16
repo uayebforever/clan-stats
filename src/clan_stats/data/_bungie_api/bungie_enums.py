a227512767dca57b1a03c029cbdf9bde71422326
Andy Green <uayebforever@gmail.com>
2026-01-15 22:27:14 -0700

    Update Bungio, Textual.

--- src/clan_stats/data/_bungie_api/bungie_enums.py
+++ src/clan_stats/data/_bungie_api/bungie_enums.py
@@ -32,90 +32,178 @@ class GameMode(EnumReprMixin, IntEnum):
     """An Enum for all available gamemodes in Destiny 2."""
 
     NONE = 0
+    """_No description given by bungie._ """
     STORY = 2
+    """_No description given by bungie._ """
     STRIKE = 3
+    """_No description given by bungie._ """
     RAID = 4
-    ALLPVP = 5
+    """_No description given by bungie._ """
+    ALL_PV_P = 5
+    """_No description given by bungie._ """
     PATROL = 6
-    ALLPVE = 7
+    """_No description given by bungie._ """
+    ALL_PV_E = 7
+    """_No description given by bungie._ """
     RESERVED9 = 9
+    """_No description given by bungie._ """
     CONTROL = 10
+    """_No description given by bungie._ """
     RESERVED11 = 11
+    """_No description given by bungie._ """
     CLASH = 12
+    """Clash -> Destiny's name for Team Deathmatch. 4v4 combat, the team with the highest kills at the end of time wins. """
     RESERVED13 = 13
-    CRIMSONDOUBLES = 15
+    """_No description given by bungie._ """
+    CRIMSON_DOUBLES = 15
+    """_No description given by bungie._ """
     NIGHTFALL = 16
-    HEROICNIGHTFALL = 17
-    ALLSTRIKES = 18
-    IRONBANNER = 19
+    """_No description given by bungie._ """
+    HEROIC_NIGHTFALL = 17
+    """_No description given by bungie._ """
+    ALL_STRIKES = 18
+    """_No description given by bungie._ """
+    IRON_BANNER = 19
+    """_No description given by bungie._ """
     RESERVED20 = 20
+    """_No description given by bungie._ """
     RESERVED21 = 21
+    """_No description given by bungie._ """
     RESERVED22 = 22
+    """_No description given by bungie._ """
     RESERVED24 = 24
-    ALLMAYHEM = 25
+    """_No description given by bungie._ """
+    ALL_MAYHEM = 25
+    """_No description given by bungie._ """
     RESERVED26 = 26
+    """_No description given by bungie._ """
     RESERVED27 = 27
+    """_No description given by bungie._ """
     RESERVED28 = 28
+    """_No description given by bungie._ """
     RESERVED29 = 29
+    """_No description given by bungie._ """
     RESERVED30 = 30
+    """_No description given by bungie._ """
     SUPREMACY = 31
-    PRIVATEMATCHESALL = 32
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_ALL = 32
+    """_No description given by bungie._ """
     SURVIVAL = 37
+    """_No description given by bungie._ """
     COUNTDOWN = 38
-    TRIALSOFTHENINE = 39
+    """_No description given by bungie._ """
+    TRIALS_OF_THE_NINE = 39
+    """_No description given by bungie._ """
     SOCIAL = 40
-    TRIALSCOUNTDOWN = 41
-    TRIALSSURVIVAL = 42
-    IRONBANNERCONTROL = 43
-    IRONBANNERCLASH = 44
-    IRONBANNERSUPREMACY = 45
-    SCOREDNIGHTFALL = 46
-    SCOREDHEROICNIGHTFALL = 47
+    """_No description given by bungie._ """
+    TRIALS_COUNTDOWN = 41
+    """_No description given by bungie._ """
+    TRIALS_SURVIVAL = 42
+    """_No description given by bungie._ """
+    IRON_BANNER_CONTROL = 43
+    """_No description given by bungie._ """
+    IRON_BANNER_CLASH = 44
+    """_No description given by bungie._ """
+    IRON_BANNER_SUPREMACY = 45
+    """_No description given by bungie._ """
+    SCORED_NIGHTFALL = 46
+    """_No description given by bungie._ """
+    SCORED_HEROIC_NIGHTFALL = 47
+    """_No description given by bungie._ """
     RUMBLE = 48
-    ALLDOUBLES = 49
+    """_No description given by bungie._ """
+    ALL_DOUBLES = 49
+    """_No description given by bungie._ """
     DOUBLES = 50
-    PRIVATEMATCHESCLASH = 51
-    PRIVATEMATCHESCONTROL = 52
-    PRIVATEMATCHESSUPREMACY = 53
-    PRIVATEMATCHESCOUNTDOWN = 54
-    PRIVATEMATCHESSURVIVAL = 55
-    PRIVATEMATCHESMAYHEM = 56
-    PRIVATEMATCHESRUMBLE = 57
-    HEROICADVENTURE = 58
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_CLASH = 51
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_CONTROL = 52
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_SUPREMACY = 53
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_COUNTDOWN = 54
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_SURVIVAL = 55
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_MAYHEM = 56
+    """_No description given by bungie._ """
+    PRIVATE_MATCHES_RUMBLE = 57
+    """_No description given by bungie._ """
+    HEROIC_ADVENTURE = 58
+    """_No description given by bungie._ """
     SHOWDOWN = 59
+    """_No description given by bungie._ """
     LOCKDOWN = 60
+    """_No description given by bungie._ """
     SCORCHED = 61
-    SCORCHEDTEAM = 62
+    """_No description given by bungie._ """
+    SCORCHED_TEAM = 62
+    """_No description given by bungie._ """
     GAMBIT = 63
-    ALLPVECOMPETITIVE = 64
+    """_No description given by bungie._ """
+    ALL_PV_E_COMPETITIVE = 64
+    """_No description given by bungie._ """
     BREAKTHROUGH = 65
-    BLACKARMORYRUN = 66
+    """_No description given by bungie._ """
+    BLACK_ARMORY_RUN = 66
+    """_No description given by bungie._ """
     SALVAGE = 67
-    IRONBANNERSALVAGE = 68
-    PVPCOMPETITIVE = 69
-    PVPQUICKPLAY = 70
-    CLASHQUICKPLAY = 71
-    CLASHCOMPETITIVE = 72
-    CONTROLQUICKPLAY = 73
-    CONTROLCOMPETITIVE = 74
-    GAMBITPRIME = 75
+    """_No description given by bungie._ """
+    IRON_BANNER_SALVAGE = 68
+    """_No description given by bungie._ """
+    PV_P_COMPETITIVE = 69
+    """_No description given by bungie._ """
+    PV_P_QUICKPLAY = 70
+    """_No description given by bungie._ """
+    CLASH_QUICKPLAY = 71
+    """_No description given by bungie._ """
+    CLASH_COMPETITIVE = 72
+    """_No description given by bungie._ """
+    CONTROL_QUICKPLAY = 73
+    """_No description given by bungie._ """
+    CONTROL_COMPETITIVE = 74
+    """_No description given by bungie._ """
+    GAMBIT_PRIME = 75
+    """_No description given by bungie._ """
     RECKONING = 76
+    """_No description given by bungie._ """
     MENAGERIE = 77
-    VEXOFFENSIVE = 78
-    NIGHTMAREHUNT = 79
+    """_No description given by bungie._ """
+    VEX_OFFENSIVE = 78
+    """_No description given by bungie._ """
+    NIGHTMARE_HUNT = 79
+    """_No description given by bungie._ """
     ELIMINATION = 80
+    """_No description given by bungie._ """
     MOMENTUM = 81
+    """_No description given by bungie._ """
     DUNGEON = 82
+    """_No description given by bungie._ """
     SUNDIAL = 83
+    """_No description given by bungie._ """
     TRIALS_OF_OSIRIS = 84
+    """_No description given by bungie._ """
     DARES = 85
+    """_No description given by bungie._ """
     OFFENSIVE = 86
-    LOSTSECTOR = 87
+    """_No description given by bungie._ """
+    LOST_SECTOR = 87
+    """_No description given by bungie._ """
     RIFT = 88
-    ZONECONTROL = 89
-    IRONBANNERRIFT = 90
-    IronBannerZoneControl = 91
-    UNKNOWN = 92
+    """_No description given by bungie._ """
+    ZONE_CONTROL = 89
+    """_No description given by bungie._ """
+    IRON_BANNER_RIFT = 90
+    """_No description given by bungie._ """
+    IRON_BANNER_ZONE_CONTROL = 91
+    """_No description given by bungie._ """
+    RELIC = 92
+    """_No description given by bungie._ """
+    LAWLESS_FRONTIER = 93
+    """_No description given by bungie._ """
+
 
 
 @final
